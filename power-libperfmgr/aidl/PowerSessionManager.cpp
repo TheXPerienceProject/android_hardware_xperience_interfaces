465b55e4370a65b9a990c3935a518ce4c15f7371
John Galt <johngaltfirstrun@gmail.com>
2024-12-17 09:40:42 -0500

    Revert "power-libperfmgr: Use deprecated AdpfProfile calls for now"
    This reverts commit 3947df50bd5bf5b3a26bf7926d07a382df358c29.

--- power-libperfmgr/aidl/PowerSessionManager.cpp
+++ power-libperfmgr/aidl/PowerSessionManager.cpp
@@ -92,8 +92,8 @@ void PowerSessionManager::updateHintMode(const std::string &mode, bool enabled) 
             mDisplayRefreshRate = 60;
         }
     }
-    if (HintManager::GetInstance()->GetAdpfProfileFromDoHint()) {
-        HintManager::GetInstance()->SetAdpfProfileFromDoHint(mode);
+    if (HintManager::GetInstance()->GetAdpfProfile()) {
+        HintManager::GetInstance()->SetAdpfProfile(mode);
     }
 }
 
